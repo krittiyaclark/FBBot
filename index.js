23a6728cb9c0aeb3f9c99bbeb8df6fbb9b371630
Krittiya Clark <krittiyaclark@gmail.com>
2017-03-04 17:30:47 -0500

    fixed (app.use(bodyParser.urlencoded({extended: false})))

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(bodyParser.urlencoded({extened: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
