ed04379439b0f01180434199b655cc13d72a431d
Krittiya Clark <krittiyaclark@gmail.com>
2017-03-04 17:24:12 -0500

    added (const app = express())

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/webhook', function(req, res){
-  if(req.query['hub.verify_token'] ===]
+  if(req.query['hub.verify_token'] ===
     'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
